b0127a505b98c255392ff877dfabd7303247f4d2
Bhumil73 <bhumil0610@gmail.com>
2021-09-12 23:22:20 +0530

    Added Hamburger menu for small screens

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: ".resume()",
+        url: "/#",
+        cName: 'nav-links'
+    },
+    {
+        title: ".projects()",
+        url: "#",
+        cName: 'nav-links'
+    },
+    {
+        title: ".blog()",
+        url: "#",
+        cName: 'nav-links'
+    },
+    {
+        title: ".vlogs()",
+        url: "#",
+        cName: 'nav-links'
+    },
+]
